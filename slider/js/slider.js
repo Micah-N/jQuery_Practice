bd4f412956b52bc7e5eeb0cf64119bef49efb65c
Micah-N <mnetz@2ndwatch.com>
2017-06-27 13:48:31 -0700

    Updating slider.js

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -9,6 +9,9 @@ $(document).ready(function(){
 function startSlider(){
 	var count = $("#slider > img").size();
 	var loop = setInterval(function(){
+		
+		if(sliderNext > count){ sliderNext = 1; sliderInt = 1;}
+			
 		$("#slider > img").fadeOut(300);
 		$("#slider > img#" + sliderNext).fadeIn(300);
 		sliderInt = sliderNext;
