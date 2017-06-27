9188cc1ccd51271382067367be8a404ca0a57f86
Micah-N <mnetz@2ndwatch.com>
2017-06-27 14:18:02 -0700

    Added files

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -18,3 +18,36 @@ function startSlider(){
 		sliderNext = sliderNext +1;
 	}, 3000);
 }
+
+function prev(){
+	var newSlide = sliderInt - 1;
+	showSlide(newSlide);
+}
+
+function next(){
+	var newSlide = sliderInt + 1;
+	showSlide(newSlide);	
+}
+
+function stopLoop(){
+	window.clearInterval();
+}
+
+function showSlide(id){
+	stopLoop();
+	if(id > count){id = 1;}
+	else if(id < 1){ id = count;}
+	
+	$("#slider > img").fadeOut(300);
+	$("#slider > img#" + id).fadeIn(300);
+	
+	sliderInt = id;
+	sliderNext = id + 1;
+	startSlider();
+}
+
+$("#slider > img").hover(function(){
+	stopLoop();
+}, function(){
+	startSlider();
+});
